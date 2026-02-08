7cff3f7aa602afc54bbbfd31d9d01bba192fa4c6
Yash Kamble <yashkamble@Yashs-MacBook-Air.local>
2026-02-08 10:45:07 +0530

    Fix firebase-admin import and add render.env

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,14 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+let serviceAccount;
+try {
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+        serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    } else {
+        serviceAccount = require("./serviceAccountKey.json");
+    }
+} catch (error) {
+    console.error("Failed to load Firebase credentials:", error);
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
